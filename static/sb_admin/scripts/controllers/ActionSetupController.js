530e8951cbd78de0ba575c90babfb03fa3bf94f8
naresh-coditas <naresh.mahajan@coditas.com>
2024-06-24 00:08:41 +0530

    Modal Dialog chnages

--- static/sb_admin/scripts/controllers/ActionSetupController.js
+++ static/sb_admin/scripts/controllers/ActionSetupController.js
@@ -1,7 +1,11 @@
 angular.module('sbAdminApp')
-	.controller('ActionSetupController', function($scope, $position, $http, API_CONFIG, $state, $stateParams) {
+	.controller('ActionSetupController', function($scope, $position, $http, API_CONFIG, $state, $stateParams, $modal) {
+		var modalInstance = null;
 		$scope.formData = {
 			id : null
+		};
+		$scope.actionUIInputData = {
+			
 		};
 		$scope.init = function() {
 			if($stateParams.actionId != ""){
@@ -22,7 +26,21 @@ angular.module('sbAdminApp')
 			});
 		};
 		
-		$scope.saveTestCase = function(){
+		$scope.fetchParameterDetails = function(parameterId){
+			var config = {
+				headers: {
+					'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken'), // Set the Authorization header with the JWT token
+					'Content-Type': 'application/json',
+					'X-MIRTH-SERVER-ID': API_CONFIG.X_MIRTH_SERVER_ID
+				}
+			};
+
+			$http.get(API_CONFIG.BASE_URL+"/actions/parameter/"+parameterId, config).success(function(response) {
+				$scope.actionUIInputData = response;
+			});
+		};
+		
+		$scope.saveTestStepAction = function(){
 			
 			var config = {
 				headers: {
@@ -36,5 +54,39 @@ angular.module('sbAdminApp')
 				$scope.formData = response;
 			});
 		};
+		
+		 $scope.openAddInputUIElement = function (fieldType) {
+            $scope.actionUIInputData.fieldType = fieldType;
+            modalInstance = $modal.open({
+							templateUrl: "static/views/pages/action-parameters-setup.html",
+							scope : $scope,
+							windowClass : 'small'
+						});
+        	};
+        
+         $scope.openEditInputUIElement = function (parameterId) {
+			$scope.fetchParameterDetails(parameterId);
+            $scope.actionUIInputData.id = parameterId;
+            modalInstance = $modal.open({
+							templateUrl: "static/views/pages/action-parameters-setup.html",
+							scope : $scope,
+							windowClass : 'small'
+						});
+        	};	
+        
+        $scope.saveActionParameter = function () {
+           var config = {
+				headers: {
+					'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken'), // Set the Authorization header with the JWT token
+					'Content-Type': 'application/json',
+					'X-MIRTH-SERVER-ID': API_CONFIG.X_MIRTH_SERVER_ID
+				}
+			};
+
+			$http.post(API_CONFIG.BASE_URL+"/actions/"+$stateParams.actionId+"/parameter", $scope.actionUIInputData, config).success(function(response) {
+				$scope.fetchActionDetails($stateParams.actionId);
+			});
+        	};
+        
 		$scope.init();
 	});
