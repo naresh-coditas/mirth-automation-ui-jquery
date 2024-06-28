c5a76eea9d52279fbb47fde9f2e201a1d6d8c600
naresh-coditas <naresh.mahajan@coditas.com>
2024-06-28 22:25:54 +0530

    Changes the Grid Icons

--- static/sb_admin/scripts/constants.js
+++ static/sb_admin/scripts/constants.js
@@ -1,5 +1,5 @@
 angular.module('sbAdminApp').constant('API_CONFIG', {
-    BASE_URL: 'http://localhost:7070',
+    BASE_URL: 'http://localhost:7575',
     TIMEOUT: 5000,
     X_MIRTH_SERVER_ID : 1
 });
